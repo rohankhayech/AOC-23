f59e3b524cf540bcdc041c785713df89559a0213
Rohan Khayech <rohankhayech@gmail.com>
2023-12-05 23:17:28 +0800

    Day 1: Trebuchet?!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,46 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // Map each line to its calibration value and take the sum.
+        return input.sumOf { line ->
+            // Combine the first and last digit to get the calibration value.
+            "${line.first { it.isDigit() }}${line.last { it.isDigit()}}"
+                .toInt()// Convert to integer.
+        }
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        // Spelled out digits and their conversions.
+        // This adds the correct digit to the string but maintains the first and last characters for overlaps.
+        val digits = mapOf(
+            "one" to "o1e",
+            "two" to "t2o",
+            "three" to "t3e",
+            "four" to "f4r",
+            "five" to "f5e",
+            "six" to "s6x",
+            "seven" to "s7n",
+            "eight" to "e8t",
+            "nine" to "n9e"
+        )
+
+        // Convert all spelled out digits.
+        return input.map {
+            var line = it
+            for (digit in digits) {
+                line = line.replace(digit.key, digit.value)
+            }
+            line
+        }.let { part1(it) }  // Use part 1 solver to find calibration value sum.
+
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_test1")
+    println("Part 1 correct?: ${part1(testInput1) == 142}")
+
+    val testInput2 = readInput("Day01_test2")
+    println("Part 1 correct?: ${part2(testInput2) == 281}")
 
     val input = readInput("Day01")
     part1(input).println()
